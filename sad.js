435c7172b687cee3a745664c62f9cf73b44cc58b
M1n007 <amin4udin@gmail.com>
2018-10-15 15:17:03 +0700

    update

--- sad.js
+++ sad.js
@@ -26,16 +26,12 @@ function getSubdomain(){
 
     var url = readline.question('masukan url example facebook.com tanpa http/https : ')
 
-    var siheaders = { 
-        'x-session-hash' : '16961ee14a95fae7bbfe69587dcca2adf647b7022e88ec6167edec568e4c69d3' 
-    };
     console.log("\n")
     console.log("======="+" "+"DOMAIN : "+url+" "+"=======")
     console.log("\n")
  
     fetch('http://35.197.130.171/?domain='+url,{
         method:'GET',
-        headers:siheaders,
     })
     .then(response => response.json())
     .then(res => {
